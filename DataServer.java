5a5baa7cfedcd6fcdc38e467dd6a56e25cb0f0d6
davidUSJ <alu.136616@usj.es>
2024-05-19 12:42:55 +0200

    Coments methods data server

--- DataServer.java
+++ DataServer.java
@@ -8,28 +8,33 @@ public class DataServer {
         alumnosDatabase = new HashMap<>();
     }
 
+    // Method POST
     public void addAlumno(Alumno alumno) {
         alumnosDatabase.put(alumno.getId(), alumno);
     }
 
+    // Not a method
     public Alumno getAlumno(int id) {
         return alumnosDatabase.get(id);
     }
 
+    // Method DELETE
     public void removeAlumno(int id) {
         alumnosDatabase.remove(id);
     }
 
+    // Method PUT
     public void updateAlumno(int id, Alumno updatedAlumno) {
         if (alumnosDatabase.containsKey(id)) {
             alumnosDatabase.put(id, updatedAlumno);
         }
     }
 
+    // Method GET
     public void printAllAlumnos() {
         for (Alumno alumno : alumnosDatabase.values()) {
             System.out.println("Alumno ID: " + alumno.getId() + ", Name: " + alumno.getName() +
-                               ", Lastname: " + alumno.getLastname() + ", Phone: " + alumno.getPhoneNumber());
+                    ", Lastname: " + alumno.getLastname() + ", Phone: " + alumno.getPhoneNumber());
         }
     }
 }
